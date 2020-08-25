79be6afb354f2d82118d3170231b965ed9acebd5
bracikaa <mehmed_duhovic@hotmail.com>
2020-08-25 22:36:56 +0200

    started work on backend, creating new folder

---
+++ frontend/build/precache-manifest.a25cb512867abad3823079e2db1e254a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aabef10e924da2e8a8f6db74b8b4d5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6ac41abbefc9d6e29fc",
+    "url": "/static/css/main.29a3ddf1.chunk.css"
+  },
+  {
+    "revision": "d15d955626069d4ba511",
+    "url": "/static/js/2.a16c46cd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a16c46cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ac41abbefc9d6e29fc",
+    "url": "/static/js/main.a6a64d41.chunk.js"
+  },
+  {
+    "revision": "13d2f7c68a09431e4e97",
+    "url": "/static/js/runtime-main.53694ef6.js"
+  }
+]);
