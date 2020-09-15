12c626129894bf80f3d65db842dc4394104f5b67
bracikaa <mehmed_duhovic@hotmail.com>
2020-09-15 13:08:33 +0200

    updating place controller

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const HttpError = require("../models/http-error");
 
 const DUMMY_PLACES = [
@@ -135,6 +135,25 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const pid = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === pid) };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === pid);
+
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });  
+};
+
+const deletePlace = (req, res, next) => {};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
