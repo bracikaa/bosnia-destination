f8179824241775183cc98b674b8067d48e0ab7bb
Mehmed <mehmed_duhovic@hotmail.com>
2020-09-26 01:49:51 +0200

    place schema

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
@@ -26,4 +27,13 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "Something went wrong!" });
 });
 
-app.listen(5000);
+mongoose
+  .connect(
+    ""
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
